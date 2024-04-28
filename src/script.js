f714aa421c3c04bbf0e2e05a78fab2348ee72aff
makoge boris <makogeboris@gmail.com>
2024-04-28 14:32:39 +0100

    modified

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const slideLeft = document.getElementById("slideLeft");
+const slideRight = document.getElementById("slideRight");
+const slide = document.querySelector(".slider");
+
+slideLeft.addEventListener("click", function () {
+  slide.style.transform = "translateX(65rem)";
+});
+
+slideRight.addEventListener("click", function () {
+  slide.style.transform = "translateX(-65rem)";
+});
